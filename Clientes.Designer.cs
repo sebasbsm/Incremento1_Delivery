d09fe0742345c56f869485078e65f6b21a5398c5
Sebastian <sebasbsm@gmail.com>
2020-09-26 16:35:50 -0500

    primer commit

---
+++ Clientes.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Delivery
+{
+    partial class Clientes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Clientes));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Registrar_Cliente = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxDireccionC = new System.Windows.Forms.TextBox();
+            this.textBoxApeMatC = new System.Windows.Forms.TextBox();
+            this.textApePatC = new System.Windows.Forms.TextBox();
+            this.textBoxNombresC = new System.Windows.Forms.TextBox();
+            this.textBoxDNIC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.textBoxCorreoC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxTelefonoC = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(124, 29);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(66, 120);
+            this.pictureBox3.TabIndex = 54;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(124, 131);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(181, 30);
+            this.pictureBox2.TabIndex = 53;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(269, 61);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(331, 51);
+            this.label8.TabIndex = 51;
+            this.label8.Text = "REGISTRARSE";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Registrar_Cliente
+            // 
+            this.Registrar_Cliente.Location = new System.Drawing.Point(295, 453);
+            this.Registrar_Cliente.Margin = new System.Windows.Forms.Padding(4);
+            this.Registrar_Cliente.Name = "Registrar_Cliente";
+            this.Registrar_Cliente.Size = new System.Drawing.Size(169, 41);
+            this.Registrar_Cliente.TabIndex = 48;
+            this.Registrar_Cliente.Text = "Registrar";
+            this.Registrar_Cliente.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(520, 355);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 17);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "Direccion";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(530, 194);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 17);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Apellido materno";
+            // 
+            // textBoxDireccionC
+            // 
+            this.textBoxDireccionC.Location = new System.Drawing.Point(453, 386);
+            this.textBoxDireccionC.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDireccionC.Name = "textBoxDireccionC";
+            this.textBoxDireccionC.Size = new System.Drawing.Size(202, 22);
+            this.textBoxDireccionC.TabIndex = 43;
+            this.textBoxDireccionC.TextChanged += new System.EventHandler(this.textBoxDireccionC_TextChanged);
+            // 
+            // textBoxApeMatC
+            // 
+            this.textBoxApeMatC.Location = new System.Drawing.Point(487, 227);
+            this.textBoxApeMatC.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxApeMatC.Name = "textBoxApeMatC";
+            this.textBoxApeMatC.Size = new System.Drawing.Size(202, 22);
+            this.textBoxApeMatC.TabIndex = 42;
+            this.textBoxApeMatC.TextChanged += new System.EventHandler(this.textBoxApeMatC_TextChanged);
+            // 
+            // textApePatC
+            // 
+            this.textApePatC.Location = new System.Drawing.Point(278, 227);
+            this.textApePatC.Margin = new System.Windows.Forms.Padding(4);
+            this.textApePatC.Name = "textApePatC";
+            this.textApePatC.Size = new System.Drawing.Size(202, 22);
+            this.textApePatC.TabIndex = 41;
+            this.textApePatC.TextChanged += new System.EventHandler(this.textApePatC_TextChanged);
+            // 
+            // textBoxNombresC
+            // 
+            this.textBoxNombresC.Location = new System.Drawing.Point(68, 227);
+            this.textBoxNombresC.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxNombresC.Name = "textBoxNombresC";
+            this.textBoxNombresC.Size = new System.Drawing.Size(202, 22);
+            this.textBoxNombresC.TabIndex = 40;
+            this.textBoxNombresC.TextChanged += new System.EventHandler(this.textBoxNombresC_TextChanged);
+            // 
+            // textBoxDNIC
+            // 
+            this.textBoxDNIC.Location = new System.Drawing.Point(124, 306);
+            this.textBoxDNIC.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxDNIC.Name = "textBoxDNIC";
+            this.textBoxDNIC.Size = new System.Drawing.Size(202, 22);
+            this.textBoxDNIC.TabIndex = 39;
+            this.textBoxDNIC.TextChanged += new System.EventHandler(this.textBoxDNIC_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(321, 194);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 17);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Apellido Paterno";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(147, 194);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 17);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 275);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 17);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "DNI";
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(165, 29);
+            this.pictureBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(140, 132);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox8.TabIndex = 52;
+            this.pictureBox8.TabStop = false;
+            // 
+            // textBoxCorreoC
+            // 
+            this.textBoxCorreoC.Location = new System.Drawing.Point(124, 386);
+            this.textBoxCorreoC.Name = "textBoxCorreoC";
+            this.textBoxCorreoC.Size = new System.Drawing.Size(202, 22);
+            this.textBoxCorreoC.TabIndex = 55;
+            this.textBoxCorreoC.TextChanged += new System.EventHandler(this.textBoxCorreoC_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(196, 355);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 17);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "Correo";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(520, 275);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 17);
+            this.label7.TabIndex = 57;
+            this.label7.Text = "Telefono";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // textBoxTelefonoC
+            // 
+            this.textBoxTelefonoC.Location = new System.Drawing.Point(453, 306);
+            this.textBoxTelefonoC.Name = "textBoxTelefonoC";
+            this.textBoxTelefonoC.Size = new System.Drawing.Size(202, 22);
+            this.textBoxTelefonoC.TabIndex = 58;
+            this.textBoxTelefonoC.TextChanged += new System.EventHandler(this.textBoxTelefonoC_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(278, 29);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(42, 102);
+            this.pictureBox1.TabIndex = 59;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Clientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(767, 533);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxTelefonoC);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCorreoC);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Registrar_Cliente);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxDireccionC);
+            this.Controls.Add(this.textBoxApeMatC);
+            this.Controls.Add(this.textApePatC);
+            this.Controls.Add(this.textBoxNombresC);
+            this.Controls.Add(this.textBoxDNIC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox8);
+            this.Name = "Clientes";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button Registrar_Cliente;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxDireccionC;
+        private System.Windows.Forms.TextBox textBoxApeMatC;
+        private System.Windows.Forms.TextBox textApePatC;
+        private System.Windows.Forms.TextBox textBoxNombresC;
+        private System.Windows.Forms.TextBox textBoxDNIC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.TextBox textBoxCorreoC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxTelefonoC;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
